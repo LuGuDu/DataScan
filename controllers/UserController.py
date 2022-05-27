8593da167ee6bfc63de446adb7ef6f2279fdad03
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-05-27 21:20:00 +0200

    estructura de gestion de usuarios

---
+++ controllers/UserController.py
@@ -0,0 +1,12 @@
+from model.User import User
+from dao.UserDAO import UserDAO
+
+def createUser(data):
+
+def deleteUser(data):
+
+def getUserList():
+
+def deleteUser(data):
+
+def updateUser(data):
---
+++ dao/UserDAO.py
@@ -0,0 +1,14 @@
+from re import A
+
+
+class UserDAO:
+
+    def save(self, mongo, user)
+
+    def getUser(self, mongo, id)
+
+    def getAll(self, mongo)
+
+    def remove(self, mongo, id)
+
+    def update(self, mongo, user)
