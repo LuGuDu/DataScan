10f6ad6517092299280ded14a5b54d140e513686
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-05-27 21:06:09 +0200

    arreglado fallo en DAO

--- dao/ModelRegistryDAO.py
+++ dao/ModelRegistryDAO.py
@@ -4,9 +4,9 @@ class ModelRegistryDAO:
         mongo.modelTrainHistorial.insert_one(modelRegistry)
 
     def getLastAttackList(self, mongo):
-        mongo.modelTrainHistorial.find_one(sort=[("date", -1)])["attack_list"]
+        return mongo.modelTrainHistorial.find_one(sort=[("date", -1)])["attack_list"]
 
     def getLastRegistry(self, mongo):
-        mongo.modelTrainHistorial.find_one(sort=[("date", -1)])
+        return mongo.modelTrainHistorial.find_one(sort=[("date", -1)])
     
     
