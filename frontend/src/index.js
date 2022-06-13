413db81bf82e4b39638239600c3ca3de340b9998
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-06-14 01:13:54 +0200

    cambio en hashrouter y añadido app.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,50 +23,12 @@ import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
 import "assets/demo/demo.css";
 
-import Index from "views/Index.js";
-
-import Welcome from "views/Welcome.js";
-
-import Login from "components/Operations/Login.js"
-import Register from "components/Operations/Register.js"
-
-import Analyze from "components/Operations/Analyze.js"
-import AttackTypesInfo from "components/Operations/AttackTypesInfo.js"
-import Contact from "components/Operations/Contact.js"
-import About from "components/Operations/About.js"
-
-import Admin from "components/Operations/Admin.js"
-import ModelInfo from "components/Admin/Model/ModelInfo.js"
-import ModelTrain from "components/Admin/Model/ModelTrain.js"
-import ModelHistory from "components/Admin/Model/ModelHistory.js"
-
-import Users from "components/Admin/Users/UserList.js"
-import UserCreateForm from "components/Admin/Users/CreateUserForm.js"
+import App from './App';
 
 
 ReactDOM.render(
   <HashRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/welcome" element={<Welcome />} />
-
-      <Route path="/analyze" element={<Analyze />} />
-      <Route path="/attacks_info" element={<AttackTypesInfo />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
-
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/admin/model/train" element={<ModelTrain />} />
-      <Route path="/admin/model/info" element={<ModelInfo />} />
-      <Route path="/admin/model/history" element={<ModelHistory />} />
-
-      <Route path="/admin/users" element={<Users />} />
-      <Route path="/admin/users/create" element={<UserCreateForm />} />
-
-    </Routes>
+    <App />
   </HashRouter>,
   document.getElementById("root")
 );
