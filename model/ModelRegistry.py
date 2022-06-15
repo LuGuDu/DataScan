d36bd725d62d1896bbefb1e380452f0e957dd6f0
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-06-15 22:41:58 +0200

    getters y setters y preparando calidad frontend

--- model/ModelRegistry.py
+++ model/ModelRegistry.py
@@ -1,5 +1,8 @@
 """Model class of ModelRegistry"""
 
+from operator import mod
+
+
 class ModelRegistry:
     """Model class of ModelRegistry"""
 
@@ -14,6 +17,78 @@ time_training, improved, accuracy, rows, columns, attack_list):
         self.accuracy = accuracy
         self.rows = rows
         self.columns = columns
+        self.attack_list = attack_list
+
+    def get_training_file_name(self):
+        """Returns the training file name"""
+        return self.training_file_name
+
+    def set_training_file_name(self, training_file_name):
+        """Set the training file name value"""
+        self.training_file_name = training_file_name
+
+    def get_date(self):
+        """Returns the date"""
+        return self.date
+
+    def set_date(self, date):
+        """Set the date value"""
+        self.date = date
+
+    def get_time_training(self):
+        """Returns the time training"""
+        return self.time_training
+
+    def set_time_training(self, time_training):
+        """Set the time training value"""
+        self.time_training = time_training
+
+    def get_model_file_name(self):
+        """Returns the model file name"""
+        return self.model_file_name
+
+    def set_model_file_name(self, model_file_name):
+        """Set the model file name value"""
+        self.model_file_name = model_file_name
+
+    def get_improved(self):
+        """Returns the improved value"""
+        return self.improved
+
+    def set_improved(self, improved):
+        """Set the improved value"""
+        self.improved = improved
+
+    def get_accuracy(self):
+        """Returns the accuracy"""
+        return self.accuracy
+
+    def set_accuracy(self, accuracy):
+        """Set the accuracy value"""
+        self.accuracy = accuracy
+
+    def get_rows(self):
+        """Returns the rows"""
+        return self.rows
+
+    def set_rows(self, rows):
+        """Set the rows values"""
+        self.rows = rows
+
+    def get_columns(self):
+        """Returns the columns"""
+        return self.columns
+
+    def set_columns(self, columns):
+        """Set the columns values"""
+        self.columns = columns
+
+    def get_attack_list(self):
+        """Returns the attack list"""
+        return self.attack_list
+
+    def set_attack_list(self, attack_list):
+        """Set the attack list values"""
         self.attack_list = attack_list
 
     def get_json(self):
--- model/User.py
+++ model/User.py
@@ -25,14 +25,30 @@ class User:
         """Returns the user username"""
         return self.username
 
+    def set_username(self, username):
+        """Set the username value"""
+        self.age = username
+
     def get_password(self):
         """Returns the user password"""
         return self.password
+
+    def set_password(self, password):
+        """Set the password value"""
+        self.password = hashlib.sha256(password.encode()).hexdigest()
 
     def get_email(self):
         """Returns the user email"""
         return self.email
 
+    def set_email(self, email):
+        """Set the email value"""
+        self.email = email
+
     def get_role(self):
         """Returns the user role"""
         return self.role
+
+    def set_role(self, role):
+        """Set the role value"""
+        self.role = role
